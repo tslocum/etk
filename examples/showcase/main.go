46aeabce93e575f0411f10ed814f2a31b9a758d5
Trevor Slocum <trevor@rocket9labs.com>
2024-10-23 19:51:53 -0700

    Add Grid to showcase

--- examples/showcase/main.go
+++ examples/showcase/main.go
@@ -128,6 +128,48 @@ func main() {
 		w.AddChildWithLabel(flexDemo, nil, "Flex")
 	}
 
+	// Grid.
+	{
+		newButton := func(i int) *etk.Button {
+			return etk.NewButton("Flexible", func() error {
+				log.Printf("Pressed button %d", i)
+				return nil
+			})
+		}
+
+		newText := func(size int) *etk.Text {
+			t := etk.NewText(fmt.Sprintf("Fixed (%dpx)", size))
+			t.SetHorizontal(etk.AlignCenter)
+			t.SetVertical(etk.AlignCenter)
+			return t
+		}
+
+		b1 := newButton(1)
+		b2 := newButton(2)
+		b3 := newButton(3)
+
+		grid := etk.NewGrid()
+		grid.SetRowSizes(75, -1, 75, -1, 150)
+
+		// First row.
+		grid.AddChildAt(newText(75), 0, 0, 2, 1)
+
+		// Second row.
+		grid.AddChildAt(b1, 0, 1, 2, 1)
+
+		// Third row.
+		grid.AddChildAt(newText(75), 0, 2, 2, 1)
+
+		// Fourth row.
+		grid.AddChildAt(b2, 0, 3, 1, 1)
+		grid.AddChildAt(b3, 1, 3, 1, 1)
+
+		// Fifth row.
+		grid.AddChildAt(newText(150), 0, 4, 2, 1)
+
+		w.AddChildWithLabel(grid, nil, "Grid")
+	}
+
 	// Input.
 	buffer := etk.NewText("Press enter to append input below to this buffer.")
 	onselected := func(text string) (handled bool) {
